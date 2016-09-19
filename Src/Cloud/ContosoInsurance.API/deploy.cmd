a13c5a603d7f3989bcaa05481887efaf9604c376
TylerLu <admin@tyler.lu>
2016-09-19 17:20:15 +0800

    Update deploy.cmd files

--- Src/Cloud/ContosoInsurance.API/deploy.cmd
+++ Src/Cloud/ContosoInsurance.API/deploy.cmd
@@ -69,7 +69,7 @@ echo Handling .NET Web Application deployment.
 
 :: 1. Restore NuGet packages
 IF /I "ContosoInsurance-Cloud.sln" NEQ "" (
-  call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\Cloud\ContosoInsurance-Cloud.sln"
+  call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\src\Cloud\ContosoInsurance-Cloud.sln"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
